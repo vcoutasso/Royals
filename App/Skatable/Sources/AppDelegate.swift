00c475e805847ec443bbf9baee3e56fcd5585be5
vcoutasso <vcoutasso@gmail.com>
2021-08-26 10:55:59 -0300

    In this house we obey the laws of view code

--- App/Skatable/Sources/AppDelegate.swift
+++ App/Skatable/Sources/AppDelegate.swift
@@ -9,7 +9,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    // swiftlint:disable all
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -30,5 +29,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-    // swiftlint:enable all
 }
