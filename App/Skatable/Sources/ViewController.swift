2497b9810ef651c92f8431b361e11f6d3790b19a
vcoutasso <vcoutasso@gmail.com>
2021-08-31 18:37:59 -0300

    Add titles to tab bar

--- App/Skatable/Sources/ViewController.swift
+++ App/Skatable/Sources/ViewController.swift
@@ -32,11 +32,21 @@ class ViewController: UIViewController {
     // MARK: - Private methods
 
     private func setupTabBarItems() {
-        tabBarItems.append(TabBarItemVC(viewController: UIViewController(), iconName: Strings.Icons.home))
-        tabBarItems.append(TabBarItemVC(viewController: MapViewController(), iconName: Strings.Icons.map))
-        tabBarItems.append(TabBarItemVC(viewController: UIViewController(), iconName: Strings.Icons.add))
-        tabBarItems.append(TabBarItemVC(viewController: UIViewController(), iconName: Strings.Icons.leaderboard))
-        tabBarItems.append(TabBarItemVC(viewController: UIViewController(), iconName: Strings.Icons.profile))
+        tabBarItems
+            .append(TabBarItemVC(viewController: UIViewController(), iconName: Strings.Icons.home,
+                                 title: Strings.Titles.home))
+        tabBarItems
+            .append(TabBarItemVC(viewController: MapViewController(), iconName: Strings.Icons.map,
+                                 title: Strings.Titles.map))
+        tabBarItems
+            .append(TabBarItemVC(viewController: UIViewController(), iconName: Strings.Icons.add,
+                                 title: Strings.Titles.add))
+        tabBarItems
+            .append(TabBarItemVC(viewController: UIViewController(), iconName: Strings.Icons.leaderboard,
+                                 title: Strings.Titles.leaderboard))
+        tabBarItems
+            .append(TabBarItemVC(viewController: UIViewController(), iconName: Strings.Icons.profile,
+                                 title: Strings.Titles.profile))
     }
 
     private func setupTabBar() {
@@ -50,6 +60,7 @@ class ViewController: UIViewController {
         for idx in items.indices {
             items[idx].image = UIImage(systemName: tabBarItems[idx].iconName)
             items[idx].selectedImage = UIImage(systemName: "\(tabBarItems[idx].iconName).fill")
+            items[idx].title = tabBarItems[idx].title
         }
     }
 
@@ -57,5 +68,6 @@ class ViewController: UIViewController {
     struct TabBarItemVC {
         let viewController: UIViewController
         let iconName: String
+        let title: String
     }
 }
