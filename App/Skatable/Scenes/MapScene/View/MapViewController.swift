730a982b2f41447133836125718bd496b07d222c
Bruno Thuma <bruno.thuma@hotmail.com>
2021-09-01 09:41:33 -0300

    MapView centers and zooms on user

--- App/Skatable/Scenes/MapScene/View/MapViewController.swift
+++ App/Skatable/Scenes/MapScene/View/MapViewController.swift
@@ -5,16 +5,22 @@
 //  Created by Bruno Thuma on 27/08/21.
 //
 
+import CoreLocation
 import MapKit
 import UIKit
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         mapView = MKMapView()
+        locationManager = CLLocationManager()
+        mapView.delegate = self
+        locationManager.delegate = self
 
         mapView.mapType = MKMapType.standard
         mapView.isZoomEnabled = true
@@ -30,5 +36,45 @@ class MapViewController: UIViewController, MKMapViewDelegate {
             mapView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
+
+        getCurrentLocation()
+    }
+
+    func getCurrentLocation() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+    func locationManager(_: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            currentLocation = location
+            mapView.showsUserLocation = true
+        }
+    }
+
+    func mapViewWillStartLocatingUser(_ mapView: MKMapView) {
+        mapView.centerToLocation(currentLocation, regionRadius: LayoutMetrics.centerToUserRegionRadius)
+    }
+
+    private enum LayoutMetrics {
+        static let centerToUserRegionRadius: CLLocationDistance = 1000
+    }
+}
+
+private extension MKMapView {
+    func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance
+    ) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius
+        )
+        setRegion(coordinateRegion, animated: true)
     }
 }
