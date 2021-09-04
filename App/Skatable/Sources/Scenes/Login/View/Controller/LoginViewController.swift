7b164a2ceb163c95b008422e2b4937b00afcc7eb
vcoutasso <vcoutasso@gmail.com>
2021-09-04 17:14:32 -0300

    Integration with firestore

--- App/Skatable/Sources/Scenes/Login/View/Controller/LoginViewController.swift
+++ App/Skatable/Sources/Scenes/Login/View/Controller/LoginViewController.swift
@@ -7,124 +7,148 @@
 
 import AuthenticationServices
 import CryptoKit
+import FirebaseAuth
+import FirebaseFirestore
 import UIKit
 
 class LoginViewController: UIViewController {
+    // Unhashed nonce.
+    fileprivate var currentNonce: String?
+
+    let appleButton: ASAuthorizationAppleIDButton = .init(type: .continue, style: .white)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupViews()
+        setupHierarchy()
+        setupConstraints()
     }
 
     private func setupViews() {
-        setupSignInButtons()
+        appleButton.translatesAutoresizingMaskIntoConstraints = false
+        appleButton.addTarget(self, action: #selector(startSignInWithAppleFlow), for: .touchUpInside)
     }
 
-    private func setupSignInButtons() {
-        let button = ASAuthorizationAppleIDButton(type: .default, style: .white)
-        button.addTarget(self, action: #selector(handleSignInWithAppleTapped), for: .touchUpInside)
+    func setupHierarchy() {
+        view.addSubview(appleButton)
+    }
 
-        view.addSubview(button)
-
-        button.snp.makeConstraints { make in
+    func setupConstraints() {
+        appleButton.snp.makeConstraints { make in
             make.centerWithinMargins.equalToSuperview()
         }
     }
 
-    @objc private func handleSignInWithAppleTapped() {
-        performSignIn()
-    }
+    @available(iOS 13, *)
+    @objc func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.email]
+        request.nonce = sha256(nonce)
 
-    private func performSignIn() {
-        let request = createAppleIDRequest()
         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-
         authorizationController.delegate = self
         authorizationController.presentationContextProvider = self
-
         authorizationController.performRequests()
     }
 
-    private func createAppleIDRequest() -> ASAuthorizationAppleIDRequest {
-        let appleIDProvider = ASAuthorizationAppleIDProvider()
-        let request = appleIDProvider.createRequest()
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap { String(format: "%02x", $0) }.joined()
 
-        request.requestedScopes = [.fullName, .email]
+        return hashString
+    }
 
-        let nonce = randomNonceString()
-        request.nonce = sha256(nonce)
-        currentNonce = nonce
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
 
-        return request
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0..<16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            randoms.forEach { random in
+                if length == 0 {
+                    return
+                }
+
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        return result
     }
 }
 
-extension LoginViewController: ASAuthorizationControllerDelegate {}
+@available(iOS 13.0, *)
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(
+        controller: ASAuthorizationController,
+        didCompleteWithAuthorization authorization: ASAuthorization
+    ) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            let credential = OAuthProvider.credential(
+                withProviderID: "apple.com",
+                idToken: idTokenString,
+                rawNonce: nonce
+            )
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if error != nil {
+                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+                    // you're sending the SHA256-hashed nonce as a hex string with
+                    // your request to Apple.
+                    print(error?.localizedDescription ?? "")
+                    return
+                }
+                guard let user = authResult?.user else { return }
+                let email = user.email ?? ""
+                guard let uid = Auth.auth().currentUser?.uid else { return }
+                let db = Firestore.firestore()
+                db.collection("User").document(uid).setData([
+                    "email": email,
+                    "uid": uid,
+                ]) { err in
+                    if let err = err {
+                        print("Error writing document: \(err)")
+                    }
+                }
+            }
+        }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
+    }
+}
 
 extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        view.window!
+        return view.window!
     }
-}
-
-// Unhashed nonce.
-private var currentNonce: String?
-
-// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
-private func randomNonceString(length: Int = 32) -> String {
-    precondition(length > 0)
-    let charset: [Character] =
-        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-    var result = ""
-    var remainingLength = length
-
-    while remainingLength > 0 {
-        let randoms: [UInt8] = (0..<16).map { _ in
-            var random: UInt8 = 0
-            let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-            if errorCode != errSecSuccess {
-                fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-            }
-            return random
-        }
-
-        randoms.forEach { random in
-            if remainingLength == 0 {
-                return
-            }
-
-            if random < charset.count {
-                result.append(charset[Int(random)])
-                remainingLength -= 1
-            }
-        }
-    }
-
-    return result
-}
-
-// @available(iOS 13, *)
-// func startSignInWithAppleFlow() {
-//  let nonce = randomNonceString()
-//  currentNonce = nonce
-//  let appleIDProvider = ASAuthorizationAppleIDProvider()
-//  let request = appleIDProvider.createRequest()
-//  request.requestedScopes = [.fullName, .email]
-//  request.nonce = sha256(nonce)
-//
-//  let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-//  authorizationController.delegate = self
-//  authorizationController.presentationContextProvider = self
-//  authorizationController.performRequests()
-// }
-
-@available(iOS 13, *)
-private func sha256(_ input: String) -> String {
-    let inputData = Data(input.utf8)
-    let hashedData = SHA256.hash(data: inputData)
-    let hashString = hashedData.compactMap {
-        String(format: "%02x", $0)
-    }.joined()
-
-    return hashString
 }
