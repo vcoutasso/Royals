7277c3b1bd4eb8cbdfcd916fc26f2ecc52f361e8
vcoutasso <vcoutasso@gmail.com>
2021-09-05 18:47:01 -0300

    Add AppleLoginService to separate responsibilities from viewcontroller

--- App/Skatable/Sources/Scenes/Login/View/Controller/LoginViewController.swift
+++ App/Skatable/Sources/Scenes/Login/View/Controller/LoginViewController.swift
@@ -6,19 +6,16 @@
 //
 
 import AuthenticationServices
-import FirebaseAuth
-import FirebaseFirestore
 import UIKit
 
 class LoginViewController: UIViewController {
-    // Unhashed nonce.
-    fileprivate var currentNonce: String?
-
     var didSendEventClosure: ((LoginViewController.Event) -> Void)?
 
-    let skelly: UIImageView = .init()
-    let textLabel: UILabel = .init()
-    let appleButton: ASAuthorizationAppleIDButton = .init(type: .continue, style: .white)
+    private lazy var skelly: UIImageView = .init()
+    private lazy var textLabel: UILabel = .init()
+    private lazy var appleButton: ASAuthorizationAppleIDButton = .init(type: .default, style: .white)
+
+    private lazy var appleLoginService: AppleLoginService = .init(contextProvider: self)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,11 +34,11 @@ class LoginViewController: UIViewController {
         textLabel.textAlignment = .center
         textLabel.lineBreakMode = .byWordWrapping
         textLabel.numberOfLines = 0
-        textLabel.font = UIFont(font: Fonts.SpriteGraffiti.regular, size: 40)
+        textLabel.font = UIFont(font: Fonts.SpriteGraffiti.regular, size: LayoutMetrics.textFontSize)
 
         appleButton.translatesAutoresizingMaskIntoConstraints = false
-        appleButton.cornerRadius = 13
-        appleButton.addTarget(self, action: #selector(startSignInWithAppleFlow), for: .touchUpInside)
+        appleButton.cornerRadius = LayoutMetrics.loginButtonCornerRadius
+        appleButton.addTarget(appleLoginService, action: #selector(appleLoginService.start), for: .touchUpInside)
     }
 
     func setupHierarchy() {
@@ -75,82 +72,16 @@ class LoginViewController: UIViewController {
         }
     }
 
-    @available(iOS 13, *)
-    @objc private func startSignInWithAppleFlow() {
-        let nonce = randomNonceString()
-        currentNonce = nonce
-        let appleIDProvider = ASAuthorizationAppleIDProvider()
-        let request = appleIDProvider.createRequest()
-        request.requestedScopes = [.email]
-        request.nonce = sha256(nonce)
-
-        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-        authorizationController.delegate = self
-        authorizationController.presentationContextProvider = self
-        authorizationController.performRequests()
-    }
-
     private enum LayoutMetrics {
         static let skellyTopOffset: CGFloat = 20
+
+        static let textFontSize: CGFloat = 40
         static let textLabelBottomOffset: CGFloat = -20
+
+        static let loginButtonCornerRadius: CGFloat = 13
         static let loginButtonHorizontalOffset: CGFloat = 45
         static let loginButtonTopOffset: CGFloat = 20
         static let loginButtonBottomOffset: CGFloat = -40
-    }
-}
-
-@available(iOS 13.0, *)
-extension LoginViewController: ASAuthorizationControllerDelegate {
-    func authorizationController(
-        controller: ASAuthorizationController,
-        didCompleteWithAuthorization authorization: ASAuthorization
-    ) {
-        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
-            guard let nonce = currentNonce else {
-                fatalError("Invalid state: A login callback was received, but no login request was sent.")
-            }
-            guard let appleIDToken = appleIDCredential.identityToken else {
-                print("Unable to fetch identity token")
-                return
-            }
-            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
-                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
-                return
-            }
-            let credential = OAuthProvider.credential(
-                withProviderID: "apple.com",
-                idToken: idTokenString,
-                rawNonce: nonce
-            )
-            Auth.auth().signIn(with: credential) { authResult, error in
-                if error != nil {
-                    // Error. If error.code == .MissingOrInvalidNonce, make sure
-                    // you're sending the SHA256-hashed nonce as a hex string with
-                    // your request to Apple.
-                    print(error?.localizedDescription ?? "")
-                    return
-                }
-                guard let user = authResult?.user else { return }
-                let email = user.email ?? ""
-                guard let uid = Auth.auth().currentUser?.uid else { return }
-                let db = Firestore.firestore()
-                db.collection("User").document(uid).setData([
-                    "email": email,
-                    "uid": uid,
-                ]) { err in
-                    if let err = err {
-                        print("Error writing document: \(err)")
-                    } else {
-                        self.didSendEventClosure?(.login)
-                    }
-                }
-            }
-        }
-    }
-
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        // Handle error.
-        print("Sign in with Apple errored: \(error)")
     }
 }
 
