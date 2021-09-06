b39b032a02587419786fb55270ca061da7c2c3e4
vcoutasso <vcoutasso@gmail.com>
2021-09-06 18:26:08 -0300

    Separate view from controller

--- App/Skatable/Sources/Scenes/MapScene/View/Controller/SearchResultsViewController.swift
+++ App/Skatable/Sources/Scenes/MapScene/View/Controller/SearchResultsViewController.swift
@@ -9,50 +9,12 @@ import SnapKit
 import UIKit
 
 class SearchResultsViewController: UITableViewController {
-    // MARK: - Private attributes
-
-    private lazy var tableHeader: UIStackView = {
-        // Label
-        let viewTitleLabel = UILabel()
-        viewTitleLabel.font = UIFont(font: Fonts.SpriteGraffiti.regular, size: LayoutMetrics.viewTitleFontSize)
-        viewTitleLabel.text = Strings.Localizable.MapScene.SearchResults.title
-
-        // Current location item
-        let locationMarker = UIImageView(image: UIImage(asset: Assets.Images.userLocationMarker))
-        locationMarker.contentMode = .scaleAspectFit
-        let subtitleText = UILabel()
-        subtitleText.text = Strings.Localizable.MapScene.SearchResults.subtitle
-        subtitleText.font = UIFont.systemFont(ofSize: LayoutMetrics.itemTitleFontSize, weight: .bold)
-        subtitleText.textColor = Assets.Colors.yellow.color
-        subtitleText.textAlignment = .center
-
-        // Hstack
-        let subtitleItem = UIStackView(arrangedSubviews: [locationMarker, subtitleText])
-        subtitleItem.axis = .horizontal
-        subtitleItem.alignment = .leading
-        subtitleItem.spacing = LayoutMetrics.itemImageToTextDistance
-
-        // Align text to center of the image
-        subtitleText.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-                .offset(-subtitleText.font.pointSize / 2)
-        }
-
-        let header = UIStackView(arrangedSubviews: [viewTitleLabel, subtitleItem])
-        header.axis = .vertical
-        header.alignment = .leading
-        header.spacing = 20
-        header.translatesAutoresizingMaskIntoConstraints = true
-
-        return header
-    }()
-
     // MARK: - Overridden methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.setAndLayoutTableHeaderView(header: tableHeader) { make in
+        tableView.setAndLayoutTableHeaderView(header: SearchResultsHeaderView()) { make in
             make.top.equalToSuperview()
                 .offset(LayoutMetrics.topHeaderOffset)
             make.leading.equalToSuperview()
@@ -63,14 +25,8 @@ class SearchResultsViewController: UITableViewController {
     // MARK: - Layout Metrics
 
     private enum LayoutMetrics {
-        static let viewTitleFontSize: CGFloat = 22
         static let topHeaderOffset: CGFloat = 30
         static let leadingOffset: CGFloat = 20
-        static let itemTitleFontSize: CGFloat = 16
-        static let itemHorizontalPadding: CGFloat = 30
-        static let itemHeight: CGFloat = 40
-        static let itemVerticalPadding: CGFloat = 15
-        static let itemImageToTextDistance: CGFloat = 30
     }
 }
 
