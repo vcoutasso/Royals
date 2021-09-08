2e3539ce01ebadd54b26ecad7310f28f373f499d
vcoutasso <vcoutasso@gmail.com>
2021-09-08 10:45:18 -0300

    Improve selection/deselection animation and fix one annotation selected at a time

--- App/Skatable/Sources/Scenes/MapScene/Data/Model/MapPinAnnotation.swift
+++ App/Skatable/Sources/Scenes/MapScene/Data/Model/MapPinAnnotation.swift
@@ -8,38 +8,43 @@
 import MapKit
 
 class MapPinAnnotation: NSObject, MKAnnotation {
-    let title: String?
-    let type: MapPinType
+    // MARK: - Public attributes
+
+    let id: UUID = .init()
+    let unselectedColor: UIColor = Assets.Colors.lightGray.color
+
     let coordinate: CLLocationCoordinate2D
+    let type: MapPinType
+    let title: String?
 
-    var isSelected: Bool = false
-
-    // TODO: Could be better
-    var image: UIImage? {
-        var img: UIImage?
-
-        switch type {
-        case .skateSpot:
-            img = UIImage(asset: Assets.Images.skateSpot)
-        case .skateStopper:
-            img = UIImage(asset: Assets.Images.skateStopper)
-        }
-
-        if isSelected {
-            switch type {
-            case .skateSpot:
-                img = img?.imageWithColor(color: Assets.Colors.green.color)
-            case .skateStopper:
-                img = img?.imageWithColor(color: Assets.Colors.red.color)
+    var isSelected: Bool = false {
+        didSet {
+            if isSelected {
+                imageView.image = imageView.image?.imageWithColor(color: selectedColor)
+            } else {
+                imageView.image = imageView.image?.imageWithColor(color: unselectedColor)
             }
+            imageView.layoutIfNeeded()
         }
-
-        return img
     }
 
+    // MARK: - Lazy variables
+
+    lazy var imageView: UIImageView = {
+        let asset = (self.type == .skateSpot) ? Assets.Images.skateSpot : Assets.Images.skateStopper
+
+        return UIImageView(image: UIImage(asset: asset)!.imageWithColor(color: unselectedColor))
+    }()
+
+    lazy var selectedColor: UIColor = {
+        (type == .skateSpot) ? Assets.Colors.green.color : Assets.Colors.red.color
+    }()
+
+    // MARK: - Initialization
+
     init(title: String, coordinate: CLLocationCoordinate2D, type: MapPinType) {
+        self.coordinate = coordinate
         self.title = title
         self.type = type
-        self.coordinate = coordinate
     }
 }
