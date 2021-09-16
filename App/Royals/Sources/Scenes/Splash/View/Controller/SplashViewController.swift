401530b952d4f85506a33436cc1e676c56e0ab5d
vcoutasso <vcoutasso@gmail.com>
2021-09-16 20:14:50 -0300

    Create splash screen view controller

--- App/Royals/Sources/Scenes/Splash/View/Controller/SplashViewController.swift
+++ App/Royals/Sources/Scenes/Splash/View/Controller/SplashViewController.swift
@@ -11,12 +11,48 @@ class SplashViewController: UIViewController {
     // MARK: - Private attributes
 
     private lazy var logoImageView: UIImageView = {
-        let logo = UIImageView()
+        let logo = UIImageView(image: UIImage(asset: Assets.Images.splashLogo))
 
         return logo
     }()
 
+    // MARK: - View controller life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupHierarchy()
+        setupConstraints()
+    }
+
+    // MARK: - Private methods
+
+    private func setupHierarchy() {
+        view.addSubview(logoImageView)
+    }
+
+    private func setupConstraints() {
+        logoImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 }
+
+#if DEBUG
+    import SwiftUI
+    struct SplashViewControllerPreview: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .previewDevice("iPhone 12")
+                .preferredColorScheme(.dark)
+        }
+
+        struct ContentView: UIViewControllerRepresentable {
+            func makeUIViewController(context: Context) -> UIViewController {
+                SplashViewController()
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+        }
+    }
+#endif
